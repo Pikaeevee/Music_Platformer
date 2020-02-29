e4090da61b530329b837de61868492b68494cc07
Angela Zhang <angelaz1@andrew.cmu.edu>
2020-02-28 19:00:17 -0500

    Enemy random sequence

---
+++ Assets/CombatManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CombatManager : MonoBehaviour
+{
+  public GameObject player;
+  public GameObject enemy;
+
+  public int numNotes;
+  private int[] enemySequence; //Array of enemy notes, ranging from 0 to 3
+  
+  // Start is called before the first frame update
+  void Start()
+  {
+    generateEnemySequence();
+    StartCoroutine(enemy.GetComponent<EnemyController>().PlayNotes(enemySequence));
+  }
+
+  void generateEnemySequence() {
+    enemySequence = new int[numNotes];
+    for(int i = 0; i < numNotes; i++) {
+      enemySequence[i] = Random.Range(0, 3);
+    }
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+      
+  }
+}
---
+++ Assets/EnemyController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+  public AudioClip[] sounds;
+
+  private AudioSource player;
+  // Start is called before the first frame update
+  void Start()
+  {
+    player = GetComponent<AudioSource>();
+  }
+
+  public IEnumerator PlayNotes(int[] notes) {
+    for(int i = 0; i < notes.Length; i++) {
+      int noteNum = notes[i];
+      Debug.Log(noteNum);
+      player.clip = sounds[noteNum];
+      player.Play();
+      yield return new WaitForSeconds(player.clip.length);
+    }
+  }
+  // Update is called once per frame
+  void Update()
+  {
+      
+  }
+}
