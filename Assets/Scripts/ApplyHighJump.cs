543e563dcf738b342447a0458244aad846e1f2e5
Silvia Chen <suiqingc@andrew.cmu.edu>
2020-03-01 01:15:09 -0500

    created playermovement and applyhighjump scripts

---
+++ Assets/Scripts/ApplyHighJump.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ApplyHighJump : MonoBehaviour
+{
+    public GameObject player;
+
+    public float duration = 10.0f; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // double jumpspeed in player movement 
+        player.GetComponent<PlayerMovement>().jumpSpeed *= 2;
+
+        StartCoroutine(JumpBuffDuration());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private IEnumerator JumpBuffDuration()
+    {
+        yield return new WaitForSeconds(duration);
+
+        // reset jumpspeed of player 
+        player.GetComponent<PlayerMovement>().jumpSpeed *= 0.5f;
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+	// PLAYER OBJECT MUST HAVE RIGIDBODY2D ATTACHED 
+	
+	public float speed = 10.0f; 
+	public float jumpSpeed = 8.0f;
+
+    private Rigidbody2D rb;
+
+    private float velocity; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        velocity = Input.GetAxis("Horizontal") * speed;
+        rb.velocity = new Vector2(velocity, rb.velocity.y); 
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb.AddForce(Vector2.up * jumpSpeed, ForceMode2D.Impulse);
+        }
+    }
+}
