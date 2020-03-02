35327b120db01b9df9cf6aec292f9d8ff24dce59
Angela Zhang <angelaz1@andrew.cmu.edu>
2020-03-02 12:51:36 -0500

    able to toggle between different music

--- Assets/Scripts/SheetMusicManager.cs
+++ Assets/Scripts/SheetMusicManager.cs
@@ -5,15 +5,18 @@ using UnityEngine.UI;
 
 public class SheetMusicManager : MonoBehaviour
 {
-  private bool showingMusic;
-
   public Sprite[] musicSprites;
   public GameObject sheetMusicImage;
   private bool[] obtainedMusic;
+  private bool showingMusic;
+  private bool recallingMusic;
+  private int currentMusicIndex;
+  private bool can_switch;
 
   void Start()
   {
     obtainedMusic = new bool[musicSprites.Length];
+    currentMusicIndex = 0;
     showingMusic = false;
     sheetMusicImage.SetActive(false);
   }
@@ -28,12 +31,36 @@ public class SheetMusicManager : MonoBehaviour
       showingMusic = true;
       sheetMusicImage.GetComponent<Image>().sprite = musicSprites[i];
       sheetMusicImage.SetActive(true);
+      currentMusicIndex = i;
+    }
+  }
+
+  public void changeToNextMusic() {
+    for(int i = 1; i < obtainedMusic.Length; i++) {
+      int newIndex = (currentMusicIndex + i) % obtainedMusic.Length;
+      if(obtainedMusic[newIndex]) {
+        sheetMusicImage.GetComponent<Image>().sprite = musicSprites[newIndex];
+        currentMusicIndex = newIndex;
+        return;
+      }
+    }
+  }
+
+  public void changeToPrevMusic() {
+    for(int i = 1; i < obtainedMusic.Length; i++) {
+      int newIndex = (currentMusicIndex + obtainedMusic.Length - i) % obtainedMusic.Length;
+      if(obtainedMusic[newIndex]) {
+        sheetMusicImage.GetComponent<Image>().sprite = musicSprites[newIndex];
+        currentMusicIndex = newIndex;
+        return;
+      }
     }
   }
 
   public void hideMusic() {
     Time.timeScale = 1;
     showingMusic = false;
+    recallingMusic = false;
     sheetMusicImage.SetActive(false);
   }
 
@@ -41,11 +68,31 @@ public class SheetMusicManager : MonoBehaviour
   {
     if(!showingMusic && Input.GetKeyDown("m")) {
       Debug.Log("Showing Music");
+      recallingMusic = true;
       displayMusic(0);
     }
     if(showingMusic && Input.GetKeyDown(KeyCode.Escape)) {
       Debug.Log("Hiding Music");
       hideMusic();
+    }
+    if(recallingMusic) {
+      if (Input.GetAxisRaw("Horizontal") < -0.5f && can_switch)
+        {
+          Debug.Log("prev");
+            changeToPrevMusic();
+            can_switch = false;
+        }
+        if (Input.GetAxisRaw("Horizontal") > 0.5f && can_switch)
+        {
+          Debug.Log("next");
+            changeToNextMusic();
+            can_switch = false;
+        }
+        if(Input.GetAxisRaw("Horizontal")<0.5f && Input.GetAxisRaw("Horizontal") > -0.5f)
+        {
+            Debug.Log("reset");
+            can_switch = true;
+        }
     }
   }
 }
