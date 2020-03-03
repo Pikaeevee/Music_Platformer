72c1560898f7e8dd11d2700795d453d6fd08dc62
Silvia Chen <suiqingc@andrew.cmu.edu>
2020-03-02 23:32:18 -0500

    created triggercombat script for enemy

--- Assets/Scripts/TriggerCombat.cs
+++ Assets/Scripts/TriggerCombat.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class TriggerCombat : MonoBehaviour
 {
+    CombatManager cm;
     // Start is called before the first frame update
     void Start()
     {
-        
+        cm = GameObject.Find("CombatManager").GetComponent<CombatManager>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        cm.Invoke("StartCombat", 0.0f);
     }
 }
