f50edf2acf759532a02c7485e614edb001d2bb53
Angela Zhang <angelaz1@andrew.cmu.edu>
2020-03-22 00:27:51 -0500

    Main Menu assets + framework implemented

---
+++ Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+    public Button playButton;
+    public Button exitButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playButton.onClick.AddListener(startGame);
+        exitButton.onClick.AddListener(exitGame);
+    }
+
+    void startGame() {
+        Debug.Log("PRESSED THE BUTTON YAY");
+        loadScene(1); //LOAD THE GAME
+    }
+
+    //Loads the given scene
+    public void loadScene(int sceneNumber)
+    {
+        SceneManager.LoadScene(sceneNumber);
+    }
+
+    //Exits the game
+    public void exitGame()
+    {
+        Application.Quit();
+    }  
+}
