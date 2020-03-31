7816051120bcf44ce1895d1709d7f8c6ab985939
Angela Zhang <angelaz1@andrew.cmu.edu>
2020-03-31 14:07:55 -0500

    UI refactoring complete; all enemies added

---
+++ Assets/Scripts/ClarenceVars.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClarenceVars : MonoBehaviour, EnemyVarsInterface
+{
+    //Gets the number of rounds the enemy will do
+    public int getRoundCount() {
+        return 1;
+    }
+
+    //Gets the fixed music note sequences of the enemy
+    public int[][] getMusicNotes() {
+        int[][] musicNotes = new int[1][];
+        musicNotes[0] = new int[]{1, 1, 2, 3, 0};
+        return musicNotes;
+    }
+
+    public string getPrebattleDialogue() {
+        return "You think a melodica will be able to match my beautiful sounds?!";
+    }
+}
---
+++ Assets/Scripts/VioletVars.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VioletVars : MonoBehaviour, EnemyVarsInterface
+{
+    //Gets the number of rounds the enemy will do
+    public int getRoundCount() {
+        return 1;
+    }
+
+    //Gets the fixed music note sequences of the enemy
+    public int[][] getMusicNotes() {
+        int[][] musicNotes = new int[1][];
+        musicNotes[0] = new int[]{0, 1, 0, 2, 0, 3, 3};
+        return musicNotes;
+    }
+
+    public string getPrebattleDialogue() {
+        return "No plastic piano can compare with the sound of a Stradivarius!";
+    }
+}
